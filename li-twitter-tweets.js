548b272cc32b683d51402c7037ef19ae1d0bb3f1
Espen Klem <espen.klem@gmail.com>
2015-03-15 21:34:15 +0100

    Gotten si.add to work. Conflict when Norch is running, but that's a minor. Seems to index faster.

--- li-twitter-tweets.js
+++ li-twitter-tweets.js
@@ -2,13 +2,15 @@
 var gsheets = require('gsheets')
 var fs = require('fs-extra')
 var ifttnorch = require('iftt-norch-tools')
+var si = require('search-index')
 
 // Read config file
-var data = fs.readFileSync('./config/config-twitter.json'), config
+var data = fs.readFileSync('/home/pi/node_modules/life-indexer/config/config-twitter.json'), config
 config = JSON.parse(data)
 
 // Get csv-file as 'data' (object)
 gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, result) {
+  console.log(result.data)
   // Iterating through rows, adding and adjusting
   for (var i=0; i<result.data.length; i++) {
     var obj = result.data[i]
@@ -20,30 +22,54 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
     obj.tags = ifttnorch.tags(obj.text)
     obj.links = ifttnorch.links(obj.text)
     obj.type = [config.type]
+    //obj = [obj]
     
     // Create JSON from object
-    var resultJSON = JSON.stringify(obj)
+    //console.log(i + '\n\n')
+    //var resultJSON = JSON.stringify(obj)
+    //var resultJSON = resultJSON.escapeUnescape()
+
+
+
+
+
+
     console.log('Object #' + i)
-    console.log(resultJSON + '\n\n\n')
+    console.log(obj + '\n\n\n')
+
+
   }
+  console.log(result.data)
+
+  si.add({'batchName': config.batchname, 'filters': config.filters}, result.data, function(err) {
+    if (!err) console.log('indexed!')
+  })
+
+
+
+
   // Create JSON from object
   //var resultJSON = JSON.stringify(result.data)
 
   
   // Escape characters that will create non-copliant JSON
   //var resultJSON = resultJSON.escapeUnescape()
+  //console.log(resultJSON)
+
+
 
   // Set file variable
   //var file = '/home/pi/lifeindex/li-twitter-tweets/twitter-tweets.json'
 
   // Write to file and read to check everything is all right (can compare to console.log before writing to confirm) 
   //fs.outputFileSync(file, resultJSON, 'utf8', function(err) {
-    //console.log(err) // => null 
+  //  console.log(err) // => null 
    
   //  fs.readFile(file, function(err, data) {
   //    console.log(resultJSON) // resultJSON 
   //  })
   //})
+
 
   //Index file with exec
   //var exec = require('child_process').exec;
