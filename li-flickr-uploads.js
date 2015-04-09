580392fed9b47aeda0e216f8e94fe99e286efc72
Espen Klem <espen.klem@gmail.com>
2015-04-09 07:33:35 +0200

    Initial file for document processor and indexer for Flickr uploads

---
+++ li-flickr-uploads.js
@@ -0,0 +1,68 @@
+// Modules and stuff required
+var gsheets = require('gsheets')
+var fs = require('fs-extra')
+var ifttnorch = require('iftt-norch-tools')
+var si = require('search-index')
+var jf = require('jsonfile')
+var util = require('util')
+var configfile = '/Users/eklem/node_modules/life-indexer/config/config-twitter.json'
+
+
+// Read config file
+config = jf.readFileSync(configfile)
+
+
+// Get csv-file as 'data' (object)
+gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, result) {
+  
+  // Check if ANY changes since last indexing process
+  if (result.updated != config.gsheetLastUpdated) {
+
+    console.log('Index is not up to date.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
+    var newTweets = []
+    var datesUpdated = []
+
+    // Iterating through rows of data from spreadsheet
+    for (var i=0; i<result.data.length; i++) {
+      var obj = result.data[i]
+
+      // Getting date and checking if indexed before
+      obj.date = ifttnorch.date(obj.date)
+      if (obj.date > config.newestItemDate) {
+        
+        // Document processing the rest
+        obj.datehuman = ifttnorch.datehuman(obj.date)
+        obj.id = ifttnorch.id(obj.date, obj.text, obj.type)
+        obj.user = ifttnorch.twitterusers(obj.user, obj.text)
+        obj.tags = ifttnorch.tags(obj.text)
+        obj.links = ifttnorch.links(obj.text)
+        obj.type = [config.type]
+        
+        // Push to the array that will be indexed + array for latest update
+        newTweets.push(obj)
+        datesUpdated.push(obj.date)
+      }
+    }
+
+    //Index newTweets and update config-file with new dates'
+    console.log(config.filters)
+    si.add({'batchName': config.batchname, 'filters': config.filters}, newTweets, function(err) {
+      if (!err) {
+        console.log('indexed!')
+        config.newestItemDate = ifttnorch.findnewestdate(datesUpdated)
+        config.gsheetLastUpdated = result.updated
+        console.dir(config)
+
+        // Write config file
+        jf.writeFileSync(configfile, config)
+      } else if (err) {
+        console.log('Error: ' + err)
+      }
+    })
+  }
+
+  // Nothing to index, just move along
+  else {
+    console.log('Index is up to date.\nNothing to index.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
+  }
+});
