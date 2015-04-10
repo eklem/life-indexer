ff1294c11f2f8f6884de8ddb5b9958b887c4fc33
Espen Klem <espen.klem@gmail.com>
2015-04-10 09:32:46 +0200

    Gmail indexer in place

--- li-gmail-starred.js
+++ li-gmail-starred.js
@@ -5,7 +5,7 @@ var ifttnorch = require('iftt-norch-tools')
 var si = require('search-index')
 var jf = require('jsonfile')
 var util = require('util')
-var configfile = '/Users/eklem/node_modules/life-indexer/config/config-twitter.json'
+var configfile = '/Users/eklem/node_modules/life-indexer/config/config-gmail-starred.json'
 
 
 // Read config file
@@ -19,7 +19,7 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
   if (result.updated != config.gsheetLastUpdated) {
 
     console.log('Index is not up to date.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
-    var newTweets = []
+    var newItems = []
     var datesUpdated = []
 
     // Iterating through rows of data from spreadsheet
@@ -33,30 +33,31 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
         // Document processing the rest
         obj.datehuman = ifttnorch.datehuman(obj.date)
         obj.id = ifttnorch.id(obj.date, obj.text, obj.type)
-        obj.user = ifttnorch.twitterusers(obj.user, obj.text)
-        obj.tags = ifttnorch.tags(obj.text)
-        obj.links = ifttnorch.links(obj.text)
+        obj.email = ifttnorch.emailaddress(obj.email)
+        obj.user = ifttnorch.emailuser(obj.email)
+        obj.gravatar = ifttnorch.emailgravatar(obj.email)
         obj.type = [config.type]
         
         // Push to the array that will be indexed + array for latest update
-        newTweets.push(obj)
+        newItems.push(obj)
         datesUpdated.push(obj.date)
       }
     }
 
-    //Index newTweets and update config-file with new dates'
-    console.log(config.filters)
-    si.add({'batchName': config.batchname, 'filters': config.filters}, newTweets, function(err) {
+    console.dir(newItems)
+
+    //Index newItems and update config-file with new dates'
+    console.dir(config)
+    si.add({'batchName': config.batchname, 'filters': config.filters}, newItems, function(err) {
       if (!err) {
         console.log('indexed!')
         config.newestItemDate = ifttnorch.findnewestdate(datesUpdated)
         config.gsheetLastUpdated = result.updated
-        console.dir(config)
 
         // Write config file
         jf.writeFileSync(configfile, config)
       } else if (err) {
-        console.log('Error: ' + err)
+        console.log('Error indexing' + err)
       }
     })
   }
@@ -65,4 +66,4 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
   else {
     console.log('Index is up to date.\nNothing to index.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
   }
-});
+})
