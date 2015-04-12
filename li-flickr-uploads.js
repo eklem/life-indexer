f65ec5288697cc99249b26ea9f40b9fd2cf6a8ba
Espen Klem <espen.klem@gmail.com>
2015-04-12 11:40:31 +0200

    First working indexer for Flickr uploads

--- li-flickr-uploads.js
+++ li-flickr-uploads.js
@@ -2,10 +2,11 @@
 var gsheets = require('gsheets')
 var fs = require('fs-extra')
 var ifttnorch = require('iftt-norch-tools')
-var si = require('search-index')
+var options = { indexPath: 'si', logLevel: 'info', logSilent: false }
+var si = require('search-index')(options)
 var jf = require('jsonfile')
 var util = require('util')
-var configfile = '/Users/eklem/node_modules/life-indexer/config/config-twitter.json'
+var configfile = '/Users/eklem/node_modules/life-indexer/config/config-flickr-uploads.json'
 
 
 // Read config file
@@ -19,7 +20,7 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
   if (result.updated != config.gsheetLastUpdated) {
 
     console.log('Index is not up to date.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
-    var newTweets = []
+    var newItems = []
     var datesUpdated = []
 
     // Iterating through rows of data from spreadsheet
@@ -32,31 +33,31 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
         
         // Document processing the rest
         obj.datehuman = ifttnorch.datehuman(obj.date)
-        obj.id = ifttnorch.id(obj.date, obj.text, obj.type)
-        obj.user = ifttnorch.twitterusers(obj.user, obj.text)
-        obj.tags = ifttnorch.tags(obj.text)
-        obj.links = ifttnorch.links(obj.text)
+        obj.text = ifttnorch.sanitizehtml(obj.text)
         obj.type = [config.type]
+        obj.id = ifttnorch.id(obj.date, obj.urlimage, obj.type)
+        obj.tags = ifttnorch.tagslist(obj.tags)
         
         // Push to the array that will be indexed + array for latest update
-        newTweets.push(obj)
+        newItems.push(obj)
         datesUpdated.push(obj.date)
       }
     }
 
-    //Index newTweets and update config-file with new dates'
-    console.log(config.filters)
-    si.add({'batchName': config.batchname, 'filters': config.filters}, newTweets, function(err) {
+    console.dir(newItems)
+
+    //Index newItems and update config-file with new dates'
+    console.dir(config)
+    si.add({'batchName': config.batchname, 'filters': config.filters}, newItems, function(err) {
       if (!err) {
         console.log('indexed!')
         config.newestItemDate = ifttnorch.findnewestdate(datesUpdated)
         config.gsheetLastUpdated = result.updated
-        console.dir(config)
 
         // Write config file
         jf.writeFileSync(configfile, config)
       } else if (err) {
-        console.log('Error: ' + err)
+        console.log('Error indexing' + err)
       }
     })
   }
@@ -65,4 +66,4 @@ gsheets.getWorksheet(config.gsheetsKey, config.gsheetsWorksheet, function(err, r
   else {
     console.log('Index is up to date.\nNothing to index.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
   }
-});
+})
